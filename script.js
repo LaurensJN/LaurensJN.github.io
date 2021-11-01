0e5f8d595d170748881176103bd622197a2cec17
Laurens Oostwegel <l.oostwegel@gmail.com>
2021-11-01 11:07:30 +0100

    updated descriptions

--- script.js
+++ script.js
@@ -8,39 +8,42 @@ function multiplyNode(node, count, deep) {
 
 //TODO get list in separate file?
 var descriptions = {
-	"QGIS" : "I used QGIS extensively during my studies and various internships. ",
-	"Revit" : "My current work involves the use of Revit for research",
-	"Blender" : "I started using Blender as a hobby project. Nowadays I use Blender and the BlenderBIM add-on for my work in BIM.",
+	"QGIS" : "I used QGIS extensively during my work, studies and various internships.",
+	"Revit" : "My current work involves the use of Revit for research in heritage BIM.",
+	"Blender" : "I started using Blender as a hobby project. Nowadays I use Blender and the BlenderBIM add-on for my work with BIM.",
 	"CloudCompare" : "As in my work and studies I often have and had to deal with point clouds, CloudCompare is no unfamiliar program to me. ",
 	"Rhino" : "Rhino+Grasshoper is an ideal combination for complex spatial analysis and visualisation. Therefore I used it not only during my \
 	BSc. Architecture, but also extensively during my MSc. Geomatics for the Built Environment.",
-	"Python" : "My daily work involves a lot of python programming. The project Sensor Vision (see section current occuptation) is mostly done with python.",
+  "FME": "I used FME for automatic LOD2 city models creation using a point cloud",
+	"Python" : "I am an experienced python programmer. It's my preferred language for data processing and manipulation. \
+  See for example <a href='https://github.com/ZAGsi/video2geoimg'>ZAGsi/video2geoimg</a> or \
+  <a href='https://github.com/ZAGsi/BlenderSLAM'>ZAGsi/BlenderSLAM</a>. I am well acustomed to opencv, pandas, scipy and many other libraries.",
 	"Csharp" : "I used C# together with Unity for my thesis of Geomatics for the Built Environment. In my thesis I used the Microsoft Hololens \
 	for indoor positioning, in the case of an emergency.",
 	"Julia" : "",
-	"SQL": "Databases are a must when working with geodata. I have experience in working with PostGIS / PostgreSQL and SpatiaLite / SQLite.",
+	"SQL": "I have experience in working with PostGIS / PostgreSQL and SpatiaLite / SQLite.",
 	"Javascript" : "I have used Javascript in my studies for efficient web visualisation of point clouds with THREE.js.",
 	"CSSHTML" : "My experience with CSS/HTML is fairly limited. However, I did include it as I have some experience, such as \
 	this website.",
-	"Geodata" : "",
-	"PointClouds" : "",
+	"Geodata" : "With Geomatics as background, my strengths - and interests - lie within the field of geodata.",
+	"PointClouds" : "We are developing a low-cost method for point cloud catpuring with stereovision. \
+                  Next to that, I used point clouds in the scan-to-BIM process.",
 	"IFC" : "Current developer of IfcOpenShell, an open source library to modify and view IFC models. See <a href='https://www.github.com/IfcOpenShell/IfcOpenShell'>IfcOpenShell</a>",
 	"CityModels": "3D city models such as CityJSON and CityGML. I am also the author of the ifccityjson converter in the IfcOpenShell \
 	library that allows for conversion from CityJSON - IFC. See <a href='https://www.github.com/IfcOpenShell/IfcOpenShell'>IfcOpenShell</a>",
-	"VersionControl" : "",
-	"overflow": "We are doing a vulnerability assessment of embankments and bridges exposed to flooding hazards. The overall objective of the oVERFLOw project \
-	is strengthening resilience to climate change impacts with the specific objective being the development of climate-resilient infrastructure. \
-	I'm working on the spatial / GIS part of the analysis in this project.",
-	"bimboost": "We created a Historic BIM model of a Slovenian heritage building in Bled. There, we measured environmental parameters \
-	and imported these and created a digital twin, to explore how this can be used for healthier environments in heritage buildings. Also, as contribution\
-	to interoperability of BIM models, the IFC format has been used for model exchange. As the existing IFC exchange schemas are not sufficient for heritage\
-	data exchange, we are in the process of developing a Model View Definition (MVD) that can be used as exchange schema for a conservation / restoration plan.",
-	"sensorvision": "We are making a low-cost SLAM system using Raspberry Pi cameras and an IMU. We will measure the accuracy of point clouds produced by the RPi's and the \
-	Stereo-PTAM algorithm, a SLAM algorithm for stereocameras. We will validate our results using LIDAR & make some suggestions when and how to use such a setup, \
-	based on our results. Follow the current progress via our github repository <a href='https://github.com/ZAGsi/blender_stereo_ptam'>ZAGsi/blender_stereo_ptam</a>.",
+	"VersionControl" : "I use github for all open source projects I am currently working on.",
+	"overflow": "- GIS data analysis and processing for vulnerability maps. <br> \
+               - Maintaining WMS / WFS QGIS server. <br> \
+               - Creator of the <a href='https://github.com/ZAGsi/video2geoimg'>ZAGsi/video2geoimg</a> repository.",
+	"bimboost": "- IFC manipulation using IfcOpenShell and BlenderBIM. <br>\
+               - Digital twins with real-time sensor data. <br>\
+               - Heritage BIM for conservation.",
+	"sensorvision": "- Using the Raspberri Pi camera module. <br> \
+                   - OpenCV for image understanding. <br> \
+                   - Low-cost SLAM system in blender. See our github repository <a href='https://github.com/ZAGsi/BlenderSLAM'>ZAGsi/BlenderSLAM</a>.,
 	"Dutch": "Dutch is my native language.",
-	"English": "I did multiple academic degrees in English and my daily work is entirely in English.",
-	"German": "My highest level of German education is B1.1. Next to that I did multiple courses at the TU Munich in German.",
+	"English": "I finished multiple academic degrees in English and I use English for my daily work.",
+	"German": "I finished multiple courses at the TU Munich in German. Currently I am taking more German lessons.",
 	"Slovene": "My highest level of Slovene education is the intermediate course Slovene at the faculty of Ljubljana.",
 }
 
